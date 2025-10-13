81ee2056596e51e63c25c2bbc8e4ba2814441b5b
Kochurin-N-S <marulik2012@gmail.com>
2025-10-13 14:49:40 +0500

    Delete Skript directory

--- Skript/lab_6.py
+++
@@ -1,5 +0,0 @@
-heroes = ['superman', 'spiderman', 'batman']
-nik, vas, ivan = heroes
-print('николай - ', nik)
-print('василий - ', vas)
-print('иван - ', ivan)
--- Skript/lab_7.py
+++
@@ -1,8 +0,0 @@
-import random
-
-a = [int(random.random() * 100) for i in range(20)]
-print(f'исходный список - {a}')
-a.sort()
-print(f'отсортированный список - {a}')
-a.pop(0)
-print(f'новый список - {a}')
