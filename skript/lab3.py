eb1654faaf74449b1c49bfc37b11d0e952a4fc8a
Kochurin-N-S <marulik2012@gmail.com>
2025-10-20 17:53:36 +0500

    Delete skript directory

--- skript/lab3.py
+++
@@ -1,4 +0,0 @@
-some_line = 'HelloWorld'
-result = tuple(some_line) #можем использовать, чтобы разделить строку посимвольно
-print(result)
-print(list(result))
--- skript/sam1.py
+++
@@ -1,5 +0,0 @@
-usr_list = input("enter your nums: ").split(' ')
-data_list = [int(i) for i in usr_list]
-data_tuple = tuple(data_list)
-print('список -', data_list)
-print('кортеж -', data_tuple)
